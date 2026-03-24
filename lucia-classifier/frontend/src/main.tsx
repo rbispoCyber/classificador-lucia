84ab6e33df0c865003523355683e0e6b0c0721b8
rbispoCyber <rbispo2003@gmail.com>
2026-03-24 18:00:54 -0300

    Cleanup: Revert manual SW registration in main.tsx (AutoUpdate mode)

--- lucia-classifier/frontend/src/main.tsx
+++ lucia-classifier/frontend/src/main.tsx
@@ -1,10 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { registerSW } from 'virtual:pwa-register';
-
-// Registra o Service Worker imediatamente para modo offline
-registerSW({ immediate: true });
-
 import './index.css'
 import App from './App.tsx'
 
